7826c9ad089bf3f4dff65baeca0db242b1fd0cdb
smonxk <thetessie312@gmail.com>
2025-10-06 23:22:49 +0200

    update2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
